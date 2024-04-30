5db0fea56b5f16c12fb5c2ec784b55ae947f70a0
Joni Laine <joni.laine.24@edu.luksia.fi>
2024-04-30 12:33:33 +0300

    update(launch_working)

--- js_rocket_harjoitukset/JS/rocket.js
+++ js_rocket_harjoitukset/JS/rocket.js
@@ -1,27 +1,28 @@
 var timer = null;
 var countdownNumber = 10;
 
-var changeState = function (state){
+var changeState = function (state) {
     document.body.className = 'body-state' + state
     clearInterval(timer);
     if (state == 2) {
         timer = setInterval (function () {
-            document.getElementById('countdown').innerHTML = 
-            countdownNumber;
-            countdownNumber = 
-            countdownNumber -1;
-        if (countdownNumber === 0) {
+            document.getElementById('countdown').innerHTML = countdownNumber;
+            countdownNumber = countdownNumber -1;
+        if (countdownNumber <=0) {
             changeState(3);
-
-        };
+        }
         }, 500);
     } else if( state == 3) {
+
+        var succes = setTimeout(function () {
             var randomNumber = Math.round (Math.random()*10);
             //well_done
-            if (randomNumber > 5){
+            if (randomNumber > 5) {
             changeState (4);
             } else {
-                changeState (5); //ohno!
+                changeState (5); 
+                //ohno!
             }
-        }
+        }, 2000);
     };
+};
