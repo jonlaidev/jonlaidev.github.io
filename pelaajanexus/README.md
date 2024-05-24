3470918dd003ad3c20d3633418759a0141891534
Joni Laine <164735249+jonlaidev@users.noreply.github.com>
2024-05-24 10:47:54 +0300

    pelaajanexus

---
+++ pelaajanexus/README.md
@@ -0,0 +1,7 @@
+# Hi!👋
+
+## reviewsite
+## media review site for a school project "pelaajanexus"
+[Link to blank](https://blank.page/)
+## 🙇 Authors
+#### Joni, Leevi & Patrik
---
+++ pelaajanexus/functions-WIP/animated-button.js
@@ -0,0 +1,22 @@
+// javascript_modal_elements
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.getElementById("close");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+ 
+// open_modal
+openModalButton.addEventListener("click", () => {
+    modal.style.display = "block"; // make_modal_visible
+    overlay.style.display = "block"; // make_overlay_visible
+});
+ 
+// close_modal
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none"; // hide_modal
+    overlay.style.display = "none"; // hide_overlay
+});
+ 
+// close_modal_by_clicking_on_overlay
+overlay.addEventListener("click", () => {
+    closeModalButton.click(); // use_same_logic_as_closing
+});
